551dd1f261da4dd8b3fd59f12295124fef427836
Luci-Lawless <lu.gmmm@gmail.com>
2017-12-04 15:19:52 +0100

    Recipe API

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { recipes, users } = require('./routes')
+
+const PORT = process.env.PORT || 3030
+
+let app = express()
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+
+  // Our routes
+  .use(recipes)
+  .use(users)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    // only print full errors in development
+    error: app.get('env') === 'development' ? err : {}
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
